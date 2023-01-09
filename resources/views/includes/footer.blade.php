b3e8b9456f85911604e4d35916a058d0f7fa27fc
faldideh <faldideh@gmail.com>
2023-01-09 12:42:03 +0700

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,52 @@
+ <!-- Footer -->
+ <footer class="section-footer mt-5 mb-4 border-top">
+    <div class="container pt-5 pb-5">
+      <div class="row justify-content-center">
+        <div class="col-12">
+          <div class="row">
+            <div class="col-12 col-lg-3">
+              <h5>FEATURES</h5>
+              <ul class="list-unstyled">
+                <li><a href="">Review</a></li>
+                <li><a href="">Community</a></li>
+                <li><a href="">Social Media Kit</a></li>
+                <li><a href="">Affiliate</a></li>
+              </ul>
+            </div>
+            <div class="col-12 col-lg-3">
+              <h5>ACCOUNT</h5>
+              <ul class="list-unstyled">
+                <li><a href="">Refund</a></li>
+                <li><a href="">Securtiy</a></li>
+                <li><a href="">Reward</a></li>
+              </ul>
+            </div>
+            <div class="col-12 col-lg-3">
+              <h5>COMPANY</h5>
+              <ul class="list-unstyled">
+                <li><a href="">Career</a></li>
+                <li><a href="">Help Center</a></li>
+                <li><a href="">Media</a></li>
+               </ul>
+            </div>
+            <div class="col-12 col-lg-3">
+              <h5>GET CONNECTED</h5>
+              <ul class="list-unstyled">
+                <li><a href="">Tanjungpandan</a></li>
+                <li><a href="">Indonesia</a></li>
+                <li><a href="">+62 819 - 9537 - 5687</a></li>
+                <li><a href="">Support@belitungku.id</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row border-top justify-content-center align-item-center pt-4">
+        <div class="col-auto text-gray-500 font-weight-light">
+          2022 Copyright Nomads • All rights reserved • Made in Tanjungpandan
+        </div>
+      </div>
+    </div>
+  </footer>
